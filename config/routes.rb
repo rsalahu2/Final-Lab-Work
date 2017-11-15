e89da6b8374a22a620b5a3c394125896f341e9b0
Rohina Salahuddin <rohina.salahuddin@gmail.com>
2017-11-15 23:42:41 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,25 @@
 Rails.application.routes.draw do
+ get 'admin' => 'admin#index'
+ controller :sessions do
+ get 'login' => :new
+ post 'login' => :create
+ delete 'logout' => :destroy
+ end
+ 
+  resources :users
+  resources :orders
+  resources :line_items
+  resources :carts
+
+  root 'store#index', as: 'store_index'
   
+  resources :products 
   
   get 'static_pages/home'
-
   get 'static_pages/help'
-
   get 'static_pages/about'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  root 'users#index'
-
+  
+  # For details on the DSL available within this file, see
+  # http://guides.rubyonrails.org/routing.html
+  
 end
